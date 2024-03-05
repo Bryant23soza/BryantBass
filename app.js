cabb4edff88db695c061d8c5a4c83684faa5929c
Bryant Soza <bryant23soza@gmail.com>
2024-03-05 12:25:09 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar-menu");
+
+menu.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+});
+
+
+// Video Player
+let listVideo = document.querySelectorAll('.video_list .vid');
+let mainVideo = document.querySelector('.main_video video');
+let title = document.querySelector('.main_video .title');
+
+listVideo.forEach(video => {
+    video.onclick = () => {
+        listVideo.forEach(vid => vid.classList.remove('active'));
+        video.classList.add('active');
+        if(video.classList.contains('active')){
+            let src = video.children[0].getAttribute('src');
+            mainVideo.src = src;
+            let text = video.children[1].innerHTML;
+            title.innerHTML = text;
+        };
+    };
+});
