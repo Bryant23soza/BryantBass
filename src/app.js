896bb33581233207aa6613c719e4d0d94a6f21d6
Bryant Soza <bryant23soza@gmail.com>
2024-06-23 13:05:52 -0700

    todays fix'

--- src/app.js
+++ src/app.js
@@ -6,23 +6,35 @@ menu.addEventListener("click", function () {
   menuLinks.classList.toggle("active");
 });
 
-
 // Video Player
-let listVideo = document.querySelectorAll('.video_list .vid');
-let mainVideo = document.querySelector('.main_video video');
-let title = document.querySelector('.main_video .title');
+let listVideo = document.querySelectorAll(".video_list .vid");
+let mainVideo = document.querySelector(".main_video video");
+let title = document.querySelector(".main_video .title");
 
-listVideo.forEach(video => {
-    video.onclick = () => {
-        listVideo.forEach(vid => vid.classList.remove('active'));
-        video.classList.add('active');
-        if(video.classList.contains('active')){
-            let src = video.children[0].getAttribute('src');
-            mainVideo.src = src;
-            let text = video.children[1].innerHTML;
-            title.innerHTML = text;
-        };
-    };
+listVideo.forEach((video) => {
+  video.onclick = () => {
+    listVideo.forEach((vid) => vid.classList.remove("active"));
+    video.classList.add("active");
+    if (video.classList.contains("active")) {
+      let src = video.children[0].getAttribute("src");
+      mainVideo.src = src;
+      let text = video.children[1].innerHTML;
+      title.innerHTML = text;
+    }
+  };
 });
 
-// hello
+const buttonEl = document.getElementById("secretbutton");
+const imgEl = document.getElementById("hidden-img");
+
+const toggleHiddenEl = (domElement) => {
+  if (domElement.style.display === "none") {
+    domElement.style.display = "block";
+  } else {
+    domElement.style.display = "none";
+  }
+};
+
+buttonEl.addEventListener("click", () => {
+  toggleHiddenEl(imgEl);
+});
