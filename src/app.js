d1b8ed6c23cb84a6c19dd929de19ebd62fde8306
Bryant Soza <121540148+Bryant23soza@users.noreply.github.com>
2025-03-05 23:40:54 -0800

    3/5

--- src/app.js
+++ src/app.js
@@ -24,34 +24,3 @@ listVideo.forEach((video) => {
     }
   };
 });
-
-// const buttonEl = document.getElementById("secretbutton");
-// const imgEl = document.getElementById("hidden-img");
-// const btnSelect = document.getElementById("secretbutton");
-// const heigthChange = document.querySelector(".main-hidden-img");
-
-// const toggleHiddenEl = (domElement) => {
-//   if (domElement.style.display === "none") {
-//     domElement.style.display = "block";
-//     btnSelect.innerHTML = "remove content";
-//     let mediaOne = window.matchMedia("(max-width: 1323px");
-//     let media = window.matchMedia("(max-width: 1024px");
-//     let mediaFour = window.matchMedia("(max-width: 475px");
-//     if (media.matches) {
-//       heigthChange.style.height = "190vh";
-//       if (mediaFour.matches) {
-//         heigthChange.style.height = "165vh";
-//       }
-//     } else {
-//       heigthChange.style.height = "55vh";
-//     }
-//   } else {
-//     domElement.style.display = "none";
-//     btnSelect.innerHTML = "More Content";
-//     heigthChange.style.height = "32vh";
-//   }
-// };
-
-// buttonEl.addEventListener("click", () => {
-//   toggleHiddenEl(imgEl);
-// });
